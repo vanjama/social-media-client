48cf0d56b871c38a3e24f3bea6f7bf19e4905e8d
vanjama <91538702+vanjama@users.noreply.github.com>
2022-11-13 19:49:34 +0100

    mocks

---
+++ src/js/storage/test.test.js
@@ -0,0 +1,17 @@
+import LocalStorageMock from "../../mocks/localStorage";
+import * as storage from "../index";
+
+global.localStorage = new LocalStorageMock();
+
+describe("storage", () => {
+  it("saves a profile object to storage", () => {
+    const key = "profile";
+    const value = {
+      name: "name",
+      email: "email",
+    };
+    const serializedValue = JSON.stringify(value);
+    storage.save(key, value);
+    expect(localStorage.getItem(key)).toEqual(serializedValue);
+  });
+});
