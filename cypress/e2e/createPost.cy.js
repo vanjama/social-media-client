07086327063a7ada1104e7606ebc0388d29027da
Vanja Marouf <91538702+vanjama@users.noreply.github.com>
2023-04-08 12:52:05 +0200

    Update createPost.cy.js

--- cypress/e2e/createPost.cy.js
+++ cypress/e2e/createPost.cy.js
@@ -1,32 +1,70 @@
-describe('validates user inputs', () => {
-  it('User can log in', () => {
-    cy.visit('/');
+const validEmail = 'cypresstest@noroff.no';
+const validPassword = 'Cypress123';
+
+const invalidEmail = 'email@email.com';
+const invalidPassword = 'pasword';
+
+describe('Creating post', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5500/');
     cy.clearLocalStorage();
-    cy.wait(1000);
-    cy.get('form#registerForm > div.modal-footer > button')
-      .contains('Login')
-      .click();
-    cy.wait(1000);
-    cy.get(
-      'form#loginForm > div.modal-body > div.form-floating > input[type=email]'
-    ).type('per@noroff.no');
-    cy.get(
-      'form#loginForm > div.modal-body > div.form-floating > input[type=password]'
-    ).type('pernoroff');
-    cy.get('form#loginForm > div.modal-footer > button')
-      .contains('Login')
-      .click();
   });
-  it('User can post', () => {
-    cy.wait(3000);
-    cy.get('#footerActions > a.btn').contains('New Post').click();
-    cy.wait(3000);
-    cy.get('#postTitle').type('Testing cypress');
-    cy.get('#postTags').type('cypress spam');
-    cy.get('#postMedia').type('https://picsum.photos/200/300?random=1');
-    cy.get('#postBody').type('Post will be deleted in 15 seconds');
-    cy.get('#postForm > div.col-12 > button').click({ force: true });
-    cy.wait(15000);
-    cy.get('div.post-actions > button').contains('Delete').click();
+  it('creates post when provided with required inputs', () => {
+    // logging in
+    cy.wait(500);
+    cy.get('#registerForm .btn-close').click();
+    cy.wait(400);
+    cy.get('button[data-auth="login"]:visible').click();
+    cy.wait(400);
+    cy.get('input[type="email"]:visible').type(validEmail);
+    cy.wait(1000);
+    cy.get('input[type="password"]:visible').type(validPassword);
+    cy.wait(1000);
+    cy.get('.btn-success:visible').click();
+    cy.wait(400);
+    cy.then(() => expect(localStorage.getItem('token')).to.not.be.null);
+    // creating post
+    cy.wait(400);
+    cy.get('.btn-outline-success:visible').click();
+    cy.wait(400);
+    cy.get('input[name="title"]').type('Hello World');
+    cy.wait(400);
+    cy.get('input[name="tags"]').type('Test');
+    cy.wait(400);
+    cy.get('textarea[name="body"]').type('Have a great evening!');
+    cy.wait(400);
+    cy.get('button[data-action="submit"]').click({ force: true });
+    cy.wait(400);
+    cy.url().should('include', 'postId');
+    cy.wait(400);
+    cy.wait(400);
+    cy.get('button[data-action="delete"]:visible').click();
+  });
+
+  it('does not create post without required inputs', () => {
+    // logging in
+    cy.wait(500);
+    cy.get('#registerForm .btn-close').click();
+    cy.wait(400);
+    cy.get('button[data-auth="login"]:visible').click();
+    cy.wait(400);
+    cy.get('input[type="email"]:visible').type(validEmail);
+    cy.wait(1000);
+    cy.get('input[type="password"]:visible').type(validPassword);
+    cy.wait(1000);
+    cy.get('.btn-success:visible').click();
+    cy.wait(400);
+    cy.then(() => expect(localStorage.getItem('token')).to.not.be.null);
+    // attempting to create post
+    cy.wait(400);
+    cy.get('.btn-outline-success:visible').click();
+    cy.wait(400);
+    cy.get('input[name="title"]').should('exist');
+    cy.wait(400);
+    cy.get('button[data-action="submit"]').click({ force: true });
+    cy.wait(400);
+    cy.url().should('not.include', 'postId');
+    cy.wait(400);
   });
 });
+
